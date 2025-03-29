00332fe3096249b3fd1393646992eed859a59add
gaurav-office <gaurav.yashikainfotech@gmail.com>
2025-03-29 18:14:36 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ const App = () => {
           <Routes>
              
             <Route path="/" element={<Index />} />
-            <Route path="/policy" element={<Policy />}
+            <Route path="/policy" element={<Policy />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
